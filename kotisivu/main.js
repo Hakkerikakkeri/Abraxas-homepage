5311d38fe866e1baf4df248dbc972f762ece6a5f
Hakkerikakkeri <88773115+Hakkerikakkeri@users.noreply.github.com>
2022-06-25 19:07:41 +0300

    Add files via upload

---
+++ kotisivu/main.js
@@ -0,0 +1,17 @@
+
+mybutton = document.getElementById("top-button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topbutton() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
